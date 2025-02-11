44ddac3a9a8491b1916902cb990a99cf8b140940
14MM4CH1N3 <n42950m@proton.me>
2025-02-10 20:45:08 -0500

    the jimmy scraper script stopped working, fixed now

--- Python/comic-grabber.py
+++ Python/comic-grabber.py
@@ -1,34 +1,35 @@
 """
 This was created as a friend and myself thought this person's comics were
-very strange and funny, so I decided to quickly write this
-I do not use discord much but I thought this was funny enough to make
-Just uses html requests to get the image link and then uses the discord
-webhook library to post to discord
+very strange and funny
 Currently this is running as a cronjob on my server
 """
 from discord_webhook import DiscordWebhook
-from lxml import html, etree
+from lxml import html
 import requests
-import re
+import datetime
 
 def get_image_link():
-	"""
-	Goes to the website that hosts his comics
-	Then gets the HTML and looks for the link for the image
-	"""
-	page = requests.get("https://www.creators.com/read/heathcliff")
-	tree = html.fromstring(page.content)
-	image = etree.tostring((tree.xpath('//*[@id="comedy-show-banner"]/a/img'))[0])
-	link = re.findall("http[s]*\S+", image.decode('utf-8'))
-	return link[0].strip("\"")
+    """
+    Goes to the website that hosts his comics
+    Then gets the HTML and looks for the link for the image
+    """
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+    headers = {"User-Agent": user_agent}
+    date = datetime.datetime.now()
+    date_str = date.strftime("%Y/%m/%d")
+    url = f"https://www.gocomics.com/heathcliff/{date_str}"
+    page = requests.get(url, headers=headers)
+    tree = html.fromstring(page.content)
+    image = tree.xpath('//meta[@property="og:image"]/@content')
+    return image[0].strip()
 
 def push_to_discord(link: str):
-	"""
-	Posts the link to the specified discord webhook
-	"""
-	webhook = DiscordWebhook("<your_webhook_link>", content = link)
-	webhook.execute()
+    """
+    Posts the link to the specified discord webhook
+    """
+    webhook = DiscordWebhook("<your_webhook_link>", content = link)
+    webhook.execute()
 
 if __name__ == "__main__":
-	link = get_image_link()
-	push_to_discord(link)
+    link = get_image_link()
+    push_to_discord(link)
